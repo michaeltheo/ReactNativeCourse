0af6105952c72e2293f323361aba0712a9f8308d
michaeltheo <michaeltheochar@gmail.com>
2026-06-08 14:13:44 +0300

    chore: update dependencies and add Tailwind CSS support
    - Added nativewind and react-native-css for styling.
    - Updated package.json with new dependencies and devDependencies.
    - Removed reset-project script to simplify project setup.
    - Created new layout and authentication screens (SignIn, SignUp).
    - Added tab screens for Insights, Settings, Subscriptions, and Onboarding.
    - Implemented global CSS with Tailwind and NativeWind configurations.
    - Set up PostCSS and Metro config for Tailwind integration.

---
+++ app/(tabs)/subcriptions.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Subcriptions = () => {
+  return (
+    <View>
+      <Text>subcriptions</Text>
+    </View>
+  );
+};
+
+export default Subcriptions;

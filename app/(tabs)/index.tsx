8daee80cac67d9d4145001b3870aaf3d27043b80
michaeltheo <michaeltheochar@gmail.com>
2026-06-08 15:14:57 +0300

    feat: enhance tab layout with dynamic insets and styling, and add theme constants

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 items-center justify-center bg-background">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
@@ -39,8 +43,8 @@ export default function App() {
         }}
         className="mt-4 rounded bg-primary px-4 py-2 text-white"
       >
-        Go to Claude's Subscription
+        Go to Claude&apos;s Subscription
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
